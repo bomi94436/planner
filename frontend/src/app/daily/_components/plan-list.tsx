616562327964df1febd7b152bf609d4751bcc6dd
bom2 <boo072963@naver.com>
2026-01-24 03:42:09 +0900

    feat: Plan 리스트 데이터 로딩 및 데이터 없음 표시

--- frontend/src/app/daily/_components/plan-list.tsx
+++ frontend/src/app/daily/_components/plan-list.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import dayjs from 'dayjs'
-import { PencilIcon, TrashIcon } from 'lucide-react'
+import { FileIcon, PencilIcon, TrashIcon } from 'lucide-react'
 import { useState } from 'react'
 import { useDebouncedCallback } from 'use-debounce'
 
@@ -21,6 +21,7 @@ import {
   ContextMenuContent,
   ContextMenuItem,
   ContextMenuTrigger,
+  Skeleton,
 } from '@/components/ui'
 import type { Plan } from '@/generated/prisma/client'
 import { useDateStore } from '@/store'
@@ -34,7 +35,7 @@ export function PlanList() {
   const [deleteTargetId, setDeleteTargetId] = useState<number | null>(null)
   const [editTargetPlan, setEditTargetPlan] = useState<Plan | null>(null)
 
-  const { data: plans } = useQuery({
+  const { data: plans, isLoading } = useQuery({
     queryKey: ['plans', selectedDate],
     queryFn: () =>
       getPlans({
@@ -74,47 +75,61 @@ export function PlanList() {
   return (
     <>
       <Card className="h-full py-4">
-        <CardContent className="space-y-2">
-          {plans?.map((plan) => {
-            const dDay = dayjs(plan.endTimestamp).diff(dayjs(selectedDate), 'day')
-            return (
-              <ContextMenu key={`plan-${plan.id}`}>
-                <ContextMenuTrigger asChild>
-                  <div
-                    className="flex items-center gap-3 hover:bg-accent hover:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground cursor-pointer rounded-md py-1 px-2"
-                    onClick={() => handlePlanClick(plan.id, !plan.completed)}
-                  >
-                    <Checkbox
-                      id={plan.id.toString()}
-                      checked={plan.completed}
-                      onCheckedChange={() => handlePlanClick(plan.id, !plan.completed)}
-                    />
-                    <label
-                      htmlFor={plan.id.toString()}
-                      className={
-                        plan.completed ? 'text-muted-foreground line-through' : 'text-foreground'
-                      }
+        <CardContent className="space-y-2 h-full">
+          {isLoading ? (
+            Array.from({ length: 5 }).map((_, index) => (
+              <Skeleton key={index} className="h-8 w-full" />
+            ))
+          ) : !plans || plans.length === 0 ? (
+            <div className="flex flex-col items-center justify-center h-full">
+              <FileIcon className="w-4 h-4 text-muted-foreground" />
+              <p className="text-muted-foreground text-sm mt-1">No Data</p>
+            </div>
+          ) : (
+            plans?.map((plan) => {
+              const dDay = dayjs(plan.endTimestamp).diff(dayjs(selectedDate), 'day')
+              return (
+                <ContextMenu key={`plan-${plan.id}`}>
+                  <ContextMenuTrigger asChild>
+                    <div
+                      className="flex items-center gap-3 hover:bg-accent hover:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground cursor-pointer rounded-md py-1 px-2"
+                      onClick={() => handlePlanClick(plan.id, !plan.completed)}
                     >
-                      {plan.title}
-                    </label>
+                      <Checkbox
+                        id={plan.id.toString()}
+                        checked={plan.completed}
+                        onCheckedChange={() => handlePlanClick(plan.id, !plan.completed)}
+                      />
+                      <label
+                        htmlFor={plan.id.toString()}
+                        className={
+                          plan.completed ? 'text-muted-foreground line-through' : 'text-foreground'
+                        }
+                      >
+                        {plan.title}
+                      </label>
 
-                    {dDay > 0 && (
-                      <span className="ml-auto text-muted-foreground text-sm">D-{dDay}</span>
-                    )}
-                  </div>
-                </ContextMenuTrigger>
+                      {dDay > 0 && (
+                        <span className="ml-auto text-muted-foreground text-sm">D-{dDay}</span>
+                      )}
+                    </div>
+                  </ContextMenuTrigger>
 
-                <ContextMenuContent>
-                  <ContextMenuItem onClick={() => setEditTargetPlan(plan)}>
-                    <PencilIcon /> Edit
-                  </ContextMenuItem>
-                  <ContextMenuItem variant="destructive" onClick={() => setDeleteTargetId(plan.id)}>
-                    <TrashIcon /> Delete
-                  </ContextMenuItem>
-                </ContextMenuContent>
-              </ContextMenu>
-            )
-          })}
+                  <ContextMenuContent>
+                    <ContextMenuItem onClick={() => setEditTargetPlan(plan)}>
+                      <PencilIcon /> Edit
+                    </ContextMenuItem>
+                    <ContextMenuItem
+                      variant="destructive"
+                      onClick={() => setDeleteTargetId(plan.id)}
+                    >
+                      <TrashIcon /> Delete
+                    </ContextMenuItem>
+                  </ContextMenuContent>
+                </ContextMenu>
+              )
+            })
+          )}
         </CardContent>
       </Card>
 

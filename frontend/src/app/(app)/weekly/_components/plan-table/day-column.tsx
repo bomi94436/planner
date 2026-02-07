e8badf7bd155bb31b35f2c2190776391e6a9d5dd
bom2 <boo072963@naver.com>
2026-02-07 16:57:34 +0900

    feat: DayColumn 컴포넌트에 실제 날짜 표시 추가

--- frontend/src/app/(app)/weekly/_components/plan-table/day-column.tsx
+++ frontend/src/app/(app)/weekly/_components/plan-table/day-column.tsx
@@ -2,25 +2,25 @@ import dayjs from 'dayjs'
 
 import { cn } from '@/lib/utils'
 import { useDateStore } from '@/store'
-import { days } from '~/weekly/_constants'
 
 export function DayColumn() {
   const selectedDate = useDateStore((state) => state.selectedDate)
+  /** 요일 배열 (일요일 ~ 토요일) */
+  const days = Array.from({ length: 7 }, (_, i) => dayjs(selectedDate).locale('ko').day(i))
 
   return (
-    <div className="grid grid-cols-7 h-10">
+    <div className="grid grid-cols-7">
       {days.map((day) => (
         <div
-          key={day}
-          className="flex items-center justify-center border-b border-r last:border-r-0 border-zinc-200"
+          key={day.format('YYYY-MM-DD')}
+          className="flex py-1 items-center justify-center border-b border-r last:border-r-0 border-zinc-200"
         >
           <span
-            className={cn('text-sm', {
-              'px-2 py-1 rounded-md bg-primary text-white':
-                day === dayjs(selectedDate).locale('ko').format('ddd'),
+            className={cn('text-sm px-2 py-1.5 font-semibold', {
+              'rounded-md bg-primary text-white': day.isSame(dayjs(selectedDate), 'day'),
             })}
           >
-            {day}
+            {day.format('ddd')} {day.format('D')}
           </span>
         </div>
       ))}
